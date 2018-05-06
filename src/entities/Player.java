02357520f326e3e6fa53f51048be89ec89771710
Dejan Trajkovic <dtrajko@gmail.com>
2018-05-06 03:37:13 +0200

    Player class basic stuff

---
+++ src/entities/Player.java
@@ -0,0 +1,16 @@
+package entities;
+
+import io.Window;
+import render.Camera;
+import world.World;
+
+public class Player {
+
+	public Player() {
+		
+	}
+	
+	public void update(float delta, Window window, Camera camera, World world) {
+		
+	}
+}
